ab5a055f6c69d1a909465c0fcc9c6d3f21996331
neisser <neisser2909@gmail.com>
2024-07-07 11:16:58 -0400

    Implementado el crud para Flight

--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IFlight } from 'src/common/interface/flight.interface';
+import { FLIGHT } from 'src/common/models/models';
+import { FlightDTO } from './dto/flight.dto';
 
 @Injectable()
-export class FlightService {}
+export class FlightService {
+    constructor(@InjectModel(FLIGHT.name) private readonly model: Model<IFlight>) { }
+
+    async create(flightDTO: FlightDTO): Promise<IFlight> {
+        const newFlight = new this.model(flightDTO);
+        return await newFlight.save();
+    }
+
+    async findAll(): Promise<IFlight[]> {
+        return await this.model.find();
+    }
+
+    async findOne(id: string): Promise<IFlight> {
+        return await this.model.findById(id);
+    }
+
+    async update(id: string, flightDTO: FlightDTO): Promise<IFlight> {
+        return await this.model.findByIdAndUpdate(id, flightDTO, { new: true });
+    }
+
+    async delete(id: string): Promise<Object> {
+        await this.model.findByIdAndDelete(id);
+        return { status: HttpStatus.OK, message: 'Deleted' }
+    }
+}
