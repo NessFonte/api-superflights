af1d4cd89e110cf1dce150ed467d70c7f9d4359c
neisser <neisser2909@gmail.com>
2025-06-03 14:00:10 -0400

    Creadas las variables de entorno del JWT

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new TimeOutInterceptor());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
