e2a62ae49a021edb1abb16431bd44bd17716f57c
neisser <neisser2909@gmail.com>
2025-06-03 14:44:25 -0400

    Creadas las rutas en el controlador de auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserDTO } from 'src/common/dto/user.dto';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('api/v1/auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @UseGuards(LocalAuthGuard)
+    @Post('sigin')
+    async singIn(@Req() req) {
+        return await this.authService.signIn(req.user);
+    }
+
+    @Post('singup')
+    async singUp(@Body() userDTO: UserDTO) {
+        return await this.authService.signUp(userDTO);
+    }
+}
